320ac657f4a0307212f3ba31d6c951e99f6d01ba
snsakib <contact@snsakib.com>
2020-04-04 04:36:28 +0600

    create logIn form layout

--- src/app/register-or-login/login/login.component.ts
+++ src/app/register-or-login/login/login.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'pm-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  logInForm = this.fb.group({
+    email: [''],
+    password: ['']
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
