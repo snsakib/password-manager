84a6b9a27870d600ef41fe87518063845f752028
snsakib <contact@snsakib.com>
2020-04-04 04:34:31 +0600

    create registration form layout

--- src/app/register-or-login/register/register.component.ts
+++ src/app/register-or-login/register/register.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'pm-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registrationForm = this.fb.group({
+    fullName: [''],
+    email: [''],
+    password: ['']
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
