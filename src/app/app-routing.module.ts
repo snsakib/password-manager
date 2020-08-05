44c322fa58a4d91c26170d36b7b2a3021506ae03
snsakib <contact@snsakib.com>
2020-08-05 08:40:02 +0600

    remove register-or-login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: "register-or-login",
-    loadChildren: () =>
-      import("./register-or-login/register-or-login.module").then(
-        m => m.RegisterOrLoginModule
-      )
-  }
+  // {
+  //   path: "register-or-login",
+  //   loadChildren: () =>
+  //     import("./register-or-login/register-or-login.module").then(
+  //       m => m.RegisterOrLoginModule
+  //     )
+  // }
 ];
 
 @NgModule({
